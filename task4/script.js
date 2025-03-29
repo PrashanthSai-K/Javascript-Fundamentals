7706ff89bc0d1f81ab915a8551e5de6a45ae6f1b
PrashanthSai-K <srisathyasai24680@gmail.com>
2025-03-29 13:11:42 +0530

    task 4 Fetch user location and show weather initially/ changes updated

--- task4/script.js
+++ task4/script.js
@@ -5,8 +5,44 @@ const header = document.querySelector(".card .header");
 const content = document.querySelector(".card .content .content-right");
 const emptyMessage = document.querySelector(".empty-message");
 
-const getWeather = async () => {
 
+const loadWeather = async () => {
+    console.log("API call");
+    header.style.display = "none";
+    document.querySelector(".content").style.display = "none";
+    emptyMessage.style.display = "block";
+
+    emptyMessage.textContent = "Loading...";
+    try {
+        navigator.geolocation.getCurrentPosition(async (pos) => {
+            const api = `https://api.weatherapi.com/v1/current.json?key=980f18311d2448dfb6c55437252403&q=${pos.coords.latitude},${pos.coords.longitude}&aqi=no`
+            const response = await fetch(api);
+            const data = await response.json();
+            header.childNodes[1].textContent = data.current.condition.text;
+            header.childNodes[3].src = `https:${data.current.condition.icon}`;
+            content.childNodes[1].textContent = ": " + data.location.name;
+            content.childNodes[3].textContent = ": " + (data.location.region || "N/A") + ", " + data.location.country;
+            content.childNodes[5].textContent = ": " + data.current.temp_c + "°C";
+            content.childNodes[7].textContent = ": " + data.current.wind_kph + " km/h";
+            content.childNodes[9].textContent = ": " + data.current.humidity + "%";
+            header.style.display = "flex";
+            document.querySelector(".content").style.display = "flex";
+            emptyMessage.style.display = "none";
+        });
+
+    } catch (error) {
+        console.log(error);
+        alert("City not found");
+        header.style.display = "none";
+        document.querySelector(".content").style.display = "none";
+        emptyMessage.style.display = "block";
+        emptyMessage.textContent = "Enter a city name to get the weather";
+        return;
+    }
+}
+
+
+const getWeather = async () => {
     console.log("API call");
     header.style.display = "none";
     document.querySelector(".content").style.display = "none";
@@ -41,4 +77,7 @@ const getWeather = async () => {
         return;
     }
 }
+
+window.onload = loadWeather;
+
 
